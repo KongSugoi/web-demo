5171c4d3ec3303263200a3646cbb8933851260ef
Kong <97668362+KongSugoi@users.noreply.github.com>
2023-09-06 18:46:13 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = 'localhost';
+$username = 'id19528554_minictf1';
+$password = 'Ze%S&px*tY#l3Eua';
+$dbname = "id19528554_minictf";
+$conn = mysqli_connect($hostname, $username, $password,$dbname);
+
+?>
